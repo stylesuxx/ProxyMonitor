9f168932f20d307bd1bdd6a5f5a4a9503cdb7c33
Chris L <stylesuxx@gmail.com>
2016-10-11 00:38:26 +0200

    Added some more documentation. Replaced 2 queus with lists

--- Monitor.py
+++ Monitor.py
@@ -28,12 +28,39 @@ class DbusProxy(dbus.service.Object):
 class Monitor(threading.Thread):
     """Monitor a proxy list."""
 
-    def __init__(self, proxy_list, discovery_workers=1, recheck_workers=1,
-                 recheck_interval=180):
+    def __init__(self, proxy_list,
+                 discovery_workers=1, recheck_workers=1,
+                 recheck_interval=180, reuse_interval=180,
+                 acquire_threshold=10):
         """Initialize the Monitor thread.
+
+        New proxies are moved to the discovered queue where workers will pick
+        them up, and if they validate, add them to the ready list.
+
+        A single worker monitors the ready list for items that have reached the
+        timeout, and moves them to the recheck queue.
+
+        Once a proxy has been used it is moved to the used list where it is
+        checked for time to bring it back in rotation and moved it to the
+        recheck queue if the time is reached.
 
         :param proxy_list: The proxy list object
         :type proxy_list: ProxyList
+
+        :param discovery_workers: Amount of workers processing the discovery
+                                  queue
+        :type discovery_workers: int
+
+        :param recheck_workers: Amount of workers processing the recheck queue
+        :type recheck_workers: int
+
+        :param recheck_interval: Recheck proxy on ready queue after this time
+                                 in seconds
+        :type recheck_interval: int
+
+        :param reuse_interval: Recheck proxy on used queue after this time
+                               in seconds
+        :type reuse_interval: int
         """
         threading.Thread.__init__(self)
 
@@ -41,58 +68,115 @@ class Monitor(threading.Thread):
 
         self.discovered = Queue()
         self.recheck = Queue()
-        self.ready = Queue()
-        self.used = Queue()
+
+        self.ready = []
+        self.used = []
 
         self.discovery_workers = discovery_workers
         self.recheck_workers = recheck_workers
         self.recheck_interval = recheck_interval
+        self.reuse_interval = reuse_interval
+        self.acquire_threshold = acquire_threshold
 
         self.done = False
         self.workers = []
 
-    def recheck_worker(self):
-        """Worker that checks proxies on the ready queue."""
-        while not self.done:
-            proxy = self.ready.get()
-            now = datetime.now()
-            delta = (now - proxy.last_check).total_seconds()
-            if delta > self.recheck_interval:
-                if proxy.validates():
-                    self.ready.put(proxy)
-                else:
-                    del self.proxy_list[str(proxy)]
-            else:
-                self.ready.put(proxy)
-                self.ready.task_done()
-                time.sleep(self.recheck_interval / 2)
-                continue
+        self.lock = threading.Lock()
 
-            self.ready.task_done()
+    def used_worker(self):
+        """Monitor the used list.
 
-    def discovery_worker(self):
-        """Worker that checks newly discovered proxies."""
+        When a proxy is older than its time to life time on the list, it is
+        moved to the recheck queue.
+        """
         while not self.done:
-            proxy = self.discovered.get()
+            # Acquire a lock while we manipulate the list
+            self.lock.acquire()
+
+            if len(self.used) > 0:
+                last_check = self.used[0].last_used
+                now = datetime.now()
+                diff = (now - last_check).total_seconds()
+                if diff > self.reuse_interval:
+                    proxy = self.used.pop()
+                    self.recheck.put(proxy)
+
+            self.lock.release()
+
+            time.sleep(1)
+
+    def ready_worker(self):
+        """Monitor the ready list.
+
+        When a proxy is above its time to life time on the list, it is moved
+        to the recheck queue.
+        """
+        while not self.done:
+            # Acquire a lock while we manipulate the list
+            self.lock.acquire()
+
+            if len(self.ready) > 0:
+
+                last_check = self.ready[0].last_check
+                now = datetime.now()
+                diff = (now - last_check).total_seconds()
+                if diff > self.recheck_interval:
+                    proxy = self.ready.pop()
+                    self.recheck.put(proxy)
+
+            self.lock.release()
+
+            time.sleep(1)
+
+    def validation_worker(self, queue):
+        """Worker that validates a proxy.
+
+        Gets a proxy from the assigned queue and validates it. If it is invalid
+        it gets removed from the system.
+
+        :param queue: The queue to process
+        :type queue: Queue
+        """
+        while not self.done:
+            proxy = queue.get()
             if proxy.validates():
-                self.ready.put(proxy)
+                self.ready.append(proxy)
             else:
+                self.lock.acquire()
+
                 del self.proxy_list[str(proxy)]
 
-            self.discovered.task_done()
+                self.lock.release()
+
+            queue.task_done()
+
+    def acquire_worker(self):
+        """Worker that acquires new proxies.
+
+        This worker acquires new proxies when the proxy list is below a
+        threshold.
+        """
+        while not self.done:
+            if len(self.proxy_list) < self.acquire_threshold:
+                self.acquire()
+
+            time.sleep(10)
 
     def acquire(self):
-        """Add newly discovered proxies to the queue."""
+        """Add newly discovered proxies to the queue.
+
+        Triggers the proxy lists acquire function and fills the discovered
+        queue.
+        """
         proxies = self.proxy_list.aquire()
         for proxy in proxies.values():
             self.discovered.put(proxy)
 
     def get_stats(self):
-        """Return proxy list statistics."""
+        """Return monitoring statistics."""
         return('%05i | %05i | %05i | %05i | %05i' %
                (len(self.proxy_list), self.discovered.qsize(),
-                self.ready.qsize(), self.recheck.qsize(),
-                self.used.qsize()))
+                len(self.ready), self.recheck.qsize(), len(self.used)))
 
     def get_ready(self):
         """Return a proxy from the ready queue.
@@ -100,16 +184,10 @@ class Monitor(threading.Thread):
         The proxy is taken from the ready queue, moved to the used queue and
         returned.
         """
-        proxy = self.ready.get()
-        self.used.put(proxy)
-        self.ready.task_done()
+        proxy = self.ready.pop()
+        self.used.append(proxy)
 
         return proxy
-
-    def stop(self):
-        """Shutdown the monitor gracefully."""
-        self.done = True
-        self.dbus_loop.quit()
 
     def run(self):
         """Run the monitor thread.
@@ -117,20 +195,34 @@ class Monitor(threading.Thread):
         It will aquire the proxy list items, and process them:
         * Aquire a list of proxy servers
         * Spawn workers to validate newly discovered
-        * Spawn workers to recheck ready
-        * Re - acquire new proxies if below threshold
+        * Spawn workers to recheck previously validated
+        * Run a worker to check the ttl on the ready list
+        * Run a worker to check the ttl on the used list
+        * Run the DBUS daemon
         """
-        self.acquire()
+        acquire_worker = threading.Thread(target=self.acquire_worker)
+        acquire_worker.daemon = True
+        acquire_worker.start()
 
         for i in range(0, self.discovery_workers):
-            worker = threading.Thread(target=self.discovery_worker)
+            worker = threading.Thread(target=self.validation_worker,
+                                      args=(self.discovered,))
             worker.daemon = True
             worker.start()
 
         for i in range(0, self.recheck_workers):
-            worker = threading.Thread(target=self.recheck_worker)
+            worker = threading.Thread(target=self.validation_worker,
+                                      args=(self.recheck,))
             worker.daemon = True
             worker.start()
+
+        ready_worker = threading.Thread(target=self.ready_worker)
+        ready_worker.daemon = True
+        ready_worker.start()
+
+        used_worker = threading.Thread(target=self.used_worker)
+        used_worker.daemon = True
+        used_worker.start()
 
         bus = dbus.SessionBus()
         bus_name = dbus.service.BusName('proxy.daemon.xxx', bus=bus)
