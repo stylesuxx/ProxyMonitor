f436d1ebe638fef097c53dddb9234846b4fb12d1
Chris L <stylesuxx@gmail.com>
2016-10-11 02:29:21 +0200

    Added testcases for https proxy

--- test/TestProxy.py
+++ test/TestProxy.py
@@ -29,3 +29,29 @@ class TestHttpProxy(unittest.TestCase):
     def proxy_name_test(self):
         print self.proxy.name
         assert self.proxy.name is 'Http'
+
+
+class TestHttpsProxy(unittest.TestCase):
+    def setUp(self):
+        self.ip = '127.0.0.1'
+        self.port = 2342
+        self.proxy = Proxy.HttpsProxy(self.ip, self.port)
+
+    def initialize_http_proxy_test(self):
+        assert self.proxy.ip is self.ip
+        assert self.proxy.port is self.port
+        assert self.proxy.added
+        assert not self.proxy.last_used
+
+    def to_string_test(self):
+        print str(self.proxy)
+        assert str(self.proxy) == '%s:%i' % (self.ip, self.port)
+
+    def validate_http_proxy_test(self):
+        self.proxy.validates()
+        assert self.proxy.checked
+        assert self.proxy.last_check
+
+    def proxy_name_test(self):
+        print self.proxy.name
+        assert self.proxy.name is 'Https'
